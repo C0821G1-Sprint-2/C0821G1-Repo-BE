85242b315a4ec4772bb3d6a12d04b76434da1e5f
tay ngo <anhtayd27111997@gmail.com>
2022-03-25 09:52:41 +0700

    Tay update cac chuc nang

--- project_2/src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
+++ project_2/src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
@@ -3,16 +3,36 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.equipment.Equipment;
 import com.example.demo.repository.IEquipmentRepository;
 import com.example.demo.service.IEmployeeService;
+import com.example.demo.service.IEquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class EquipmentServiceImpl implements IEmployeeService {
+public class EquipmentServiceImpl implements IEmployeeService, IEquipmentService {
     @Autowired
     IEquipmentRepository iEquipmentRepository;
 
     @Override
     public Equipment findById(Integer id) {
         return iEquipmentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Optional<Equipment> findEquipmentById(Integer id) {
+        return this.iEquipmentRepository.findEquipmentById(id);
+    }
+
+    @Override
+    public Page<Equipment> findAllEquipmentByKeyword(String keyword, Pageable pageable) {
+        return this.iEquipmentRepository.findAllContractByKeyword(keyword,pageable);
+    }
+
+    @Override
+    public void deleteEquipment(Integer id) {
+        this.iEquipmentRepository.deleteEquipment(id);
     }
 }
