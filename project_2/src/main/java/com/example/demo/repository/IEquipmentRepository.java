8302d6abc8e75622c0decce6954823c3e4d0d155
tay ngo <anhtayd27111997@gmail.com>
2022-03-25 11:15:18 +0700

    update

--- project_2/src/main/java/com/example/demo/repository/IEquipmentRepository.java
+++ project_2/src/main/java/com/example/demo/repository/IEquipmentRepository.java
@@ -26,12 +26,11 @@ public interface IEquipmentRepository extends JpaRepository<Equipment,Integer> {
     Optional<Equipment> findEquipmentById(Integer id);
 
     @Query(value = " select * from equipment \n" +
-            " where equipment.code like %?1% and equipment.delete_flag = 0 " +
+            " where equipment.code like %?1% or equipment.expired like %?1% or equipment.name like %?1% or equipment.price like %?1% and equipment.delete_flag = 0 " +
             " order by equipment.id desc ", nativeQuery = true,
             countQuery = " select count(*) from equipment \n " +
-                    " where equipment.code like %?1% and equipment.delete_flag = 0 " +
+                    " where equipment.code like %?1% or equipment.expired like %?1% or equipment.name like %?1% or equipment.price like %?1% and equipment.delete_flag = 0 " +
                     " order by equipment.id desc ")
     Page<Equipment> findAllContractByKeyword(String keyword,@Param("page") Pageable pageable);
-
 
 }
