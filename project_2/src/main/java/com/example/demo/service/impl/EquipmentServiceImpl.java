40c46f4d019446711f5f515b88fb83555b137317
Gia Bảo <nguyenhoanggiabao1993@gmail.com>
2022-03-25 10:18:48 +0700

    Bảo 25/03

--- project_2/src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
+++ project_2/src/main/java/com/example/demo/service/impl/EquipmentServiceImpl.java
@@ -1,18 +1,22 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.employee.Employee;
 import com.example.demo.entity.equipment.Equipment;
 import com.example.demo.repository.IEquipmentRepository;
 import com.example.demo.service.IEmployeeService;
+import com.example.demo.service.IEquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EquipmentServiceImpl implements IEmployeeService {
+public class EquipmentServiceImpl implements IEquipmentService {
     @Autowired
     IEquipmentRepository iEquipmentRepository;
+
 
     @Override
     public Equipment findById(Integer id) {
         return iEquipmentRepository.findById(id).orElse(null);
     }
+
 }
